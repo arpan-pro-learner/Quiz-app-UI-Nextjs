fc3b196a41adbf0d04d8a4f6274b4465600794ce
Your Name <aryantech2002@gmail.com>
2024-03-31 00:06:07 +0530

    changed the favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <link rel="icon" href="https://thumbs.dreamstime.com/z/quiz-question-marks-sign-icon-questions-answers-game-symbol-classic-flat-vector-181034856.jpg" sizes="any" />
     </html>
   );
 }
